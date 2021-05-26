c6b73d7ba8d4a0fc41d764f0c73ebbfc7ffba53d
ta2lor <ksh262011@naver.com>
2021-05-26 22:52:55 +0900

    0526 밝기조절 기능 추가

--- app/src/main/java/com/example/chosungproj/SettingActivity.java
+++ app/src/main/java/com/example/chosungproj/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.example.chosungproj;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,36 @@ public class SettingActivity  extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_view);
+
+
+        SeekBar ScreenLightBar = findViewById(R.id.ScreenLight);
+
+        ScreenLightBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                setBrightness(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void setBrightness(int value) {
+        if(value<10){
+            value=10;
+        }else if(value > 100){
+            value = 100;
+        }
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.screenBrightness =(float) value/100;
+        getWindow().setAttributes(params);
     }
 }
