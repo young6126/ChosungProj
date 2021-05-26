f8956c7b779aa30242a3522d82d3878c4b285645
ta2lor <ksh262011@naver.com>
2021-05-26 22:36:42 +0900

    0526메인화면 생성 및 화면전환

--- app/src/main/java/com/example/chosungproj/MainActivity.java
+++ app/src/main/java/com/example/chosungproj/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.chosungproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this is push test 2
+        Button settingButton = (Button)findViewById(R.id.setting);
+        Button chosungButton = (Button)findViewById(R.id.chosunggame);
+        Button wordButton = (Button)findViewById(R.id.wordgame);
+        Button recordButton = (Button)findViewById(R.id.record);
+
+        settingButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+        chosungButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), ChosungGameActivity.class);
+                startActivity(intent);
+            }
+        });
+        wordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), WordGameActivity.class);
+                startActivity(intent);
+            }
+        });
+        recordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), RecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
