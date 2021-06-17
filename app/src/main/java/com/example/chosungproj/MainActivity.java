6616f4763e5b36d868229616964c01f16c97c38c
Han-Byeong-Geun <Han-Byeong-Geun:wqexit>
2021-06-17 17:37:22 +0100

    pull

--- app/src/main/java/com/example/chosungproj/MainActivity.java
+++ app/src/main/java/com/example/chosungproj/MainActivity.java
@@ -1,26 +1,11 @@
 package com.example.chosungproj;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Map;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
